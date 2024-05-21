d455d056cc337f56f88d43a7e7d81a70309bb120
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-05-21 20:57:49 -0300

    att

--- automatizador.py
+++ automatizador.py
@@ -3,8 +3,18 @@ import time
 
 # Lista de números de telefone (inclua o código do país, ex: +5511999999999 para Brasil)
 phone_numbers = [
+
+    
     "+5521999999999",
-    "+5521999999999"
+    "+5521999999999",
+    "+5521999999999",
+    "+5521999999999",
+    "+5521999999999",
+    "+5521999999999",
+    "+5521999999999",
+    "+5521999999999",
+    "+5521999999999",
+    
     
 ]
 
@@ -12,16 +22,17 @@ phone_numbers = [
 message = "Olá! Esta é uma mensagem automatizada enviada pelo script em Python."
 
 # Tempo de atraso entre mensagens (em segundos)
-delay_between_messages = 5
+delay_between_messages = 15
 
 # Função para enviar mensagem
 def send_whatsapp_message(phone_number, message):
     kit.sendwhatmsg_instantly(phone_number, message)
-    time.sleep(4)  # Tempo para o navegador abrir e enviar a mensagem
+    time.sleep(5)  # Tempo para o navegador abrir e enviar a mensagem
 
 # Enviando mensagem para todos os números na lista
 for number in phone_numbers:
     send_whatsapp_message(number, message)
+
     time.sleep(delay_between_messages)  # Espera para evitar problemas com envio rápido demais
 
 print("Mensagens enviadas com sucesso!")
